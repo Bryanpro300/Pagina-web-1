fe900c519b3c83bc96f3c108f909f7cda851fd0c
Bryanpro300 <bryanm00111@gmail.com>
2023-03-30 19:03:13 -0600

    first commit

---
+++ js/abstraccion.js
@@ -0,0 +1,23 @@
+//  let Personal = {
+//    nombre: "Brayan",
+//    apellido: "Miranda"
+
+    //  mostrarNombre: function () {
+
+    //    return 'Mi nombre es ${this.nombre} ${this.apellido'
+    //  }
+// }
+// 
+// 
+// 
+// 
+// 
+function Persona() {
+   this.nombre = nombre,
+   this.apellido = apellido,
+   this.mostrarNombre = function () {
+    return 'Mi nombre es ${this.nombre} ${this.apellido}';
+   }
+}
+
+let persona1 = new Persona("Brayan","Miranda");
